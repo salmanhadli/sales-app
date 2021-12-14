d400cfa1c18c3a05da76538900e470f179a3c977
Salman Hadli <skh174211@gmail.com>
2021-12-14 18:29:42 +0530

    remove comments on APIs file

--- src/Util/APIs.js
+++ src/Util/APIs.js
@@ -1,26 +1,3 @@
-// import axios from "axios";
-
-// export const getTableData = async (url) => {
-//   let data;
-//   let totalRecords;
-
-//   axios.get(url).then((res) => {
-//     console.log(res);
-//     if (!res.data.data) {
-//       data = res.data;
-//       totalRecords = res.data.length;
-//     } else {
-//       data = res.data.data;
-//       totalRecords = res.data.length;
-//     }
-//   });
-
-//   console.log(data);
-//   console.log(totalRecords);
-
-//   return { data, totalRecords };
-// };
-
 import axios from "axios";
 
 export const getTableData = async (url) => {
